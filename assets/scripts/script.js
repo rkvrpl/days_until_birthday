0c0b099e1b87b8dcccef33daa865238145b84278
rkvrpl <rkvrpl@icloud.com>
2023-11-30 16:37:04 +0300

    added function on button

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,10 +2,29 @@
 
 const date = document.getElementById('date');
 const button = document.getElementById('button');
-const result = document.querySelector('.result');
+const resultDays = document.querySelector('.result');
 
 button.addEventListener('click', function(){
-    const dateOfbirth = date.value;
+    const dateOfbirth = new Date(date.value);
     const currentDate = new Date();
-    console.log(dateOfbirth);
+    const diff = dateOfbirth - currentDate;
+    const result = new Date(diff);
+    const days = Math.round(result.getDate());
+
+    let nameDay = '';
+    if (days > 10 && [11, 12, 13, 14].includes(days%100)) {
+        nameDay = ' дней';
+    }
+    else if ((days % 10) === 1){
+        nameDay = ' день';
+    }
+    else if((days % 10) === 2 && (days % 10) === 3 && (days % 10) === 4){
+        nameDay = ' дня';
+    }
+    else{
+        nameDay = ' дней';
+    }
+    
+    resultDays.textContent = 'До вашего дня рождения осталось ' + days + nameDay; 
 });
+
