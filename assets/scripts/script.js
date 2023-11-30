039b02dc13dcd4e3caca7b3d12b78ea07536b30a
rkvrpl <rkvrpl@icloud.com>
2023-11-30 17:14:46 +0300

    added styles

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,28 +3,38 @@
 const date = document.getElementById('date');
 const button = document.getElementById('button');
 const resultDays = document.querySelector('.result');
+const currentDate = new Date();
 
 button.addEventListener('click', function(){
-    const dateOfbirth = new Date(date.value);
-    const currentDate = new Date();
-    const diff = dateOfbirth - currentDate;
-    const result = new Date(diff);
-    const days = Math.round(result.getDate());
+    const dateOfbirth = Date.parse(date.value);
+    const parsedDate = Date.parse(currentDate);
+    const diff = dateOfbirth - parsedDate;
+    const diffDays = Math.ceil(diff / 1000 / 3600 / 24);
+    console.log(diffDays);
 
     let nameDay = '';
-    if (days > 10 && [11, 12, 13, 14].includes(days%100)) {
+    if (diffDays > 10 && [11, 12, 13, 14].includes(diffDays%100)) {
         nameDay = ' дней';
     }
-    else if ((days % 10) === 1){
+    else if ((diffDays % 10) === 1){
         nameDay = ' день';
     }
-    else if((days % 10) === 2 && (days % 10) === 3 && (days % 10) === 4){
+    else if((diffDays % 10) === 2 && (diffDays % 10) === 3 && (diffDays % 10) === 4){
         nameDay = ' дня';
     }
     else{
         nameDay = ' дней';
     }
+
+    if(diffDays < 0){
+        resultDays.textContent = `Ваш день рождения в этом году уже прошел :)`; 
+    }
+    else if (diffDays === 0) {
+        resultDays.textContent =`Ваш день рождения сегодня!`;
+    } 
+    else {
+        resultDays.textContent = `До вашего дня рождения ${diffDays} ${nameDay}!`;
+    }
     
-    resultDays.textContent = 'До вашего дня рождения осталось ' + days + nameDay; 
 });
 
